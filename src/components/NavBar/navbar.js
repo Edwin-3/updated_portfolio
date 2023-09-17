2adaceae116d9ba3d13136b86f85b46589e3c161
Edwin Mutua <odoo@softiqtechnologies.com>
2023-09-17 18:10:22 +0300

    NavBar and theme components

---
+++ src/components/NavBar/navbar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './navbar.scss'
+
+const Navbar = () => {
+    return (
+        <div className='container nav-bar'>
+            <a href='/' className='logo'>
+                <h2>Edwin Mutua</h2>
+            </a>
+            <nav>
+                <a href='/#about' >About</a>
+                <a href='/#portfolio' >Work</a>
+                <a href='#contact'>Contact</a>
+                <a href='' download>Resume</a>
+            </nav>
+            <button className='btn'>Let's Talk</button>
+
+        </div>
+    )
+}
+
+export default Navbar
